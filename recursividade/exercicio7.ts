266358cfc5e6a43de3ce8298bb13329e73fc7bf2
Murilo Marcal <Murilo.Marcal@StoneX.com>
2025-04-14 21:09:22 -0300

    simplify factorial

--- recursividade/exercicio7.ts
+++ recursividade/exercicio7.ts
@@ -6,12 +6,7 @@ function exercicio7(n: number, newN: number, sum: number)
         return;
     }
 
-
-    let calc = newN * sum;
-
-    console.log(newN + " * " + sum + " = " + calc)
-
-    exercicio7(n, newN - 1, calc);
+    exercicio7(n, newN - 1, newN * sum);
 }
 
 exercicio7(7, 7, 1);
